50fafa232fe63c9d4b42d2d1dd9f8c6decc9e782
Kubilay <meneskaytemiz@hotmail.com>
2020-07-15 20:05:53 +0300

    rename

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'knopy',         
-  packages = ['knopy'],   
-  version = '0.0.0',      
+  name = 'scicalc',         
+  packages = ['scicalc'],   
+  version = '0',      
   license='MIT',       
   description = 'A scientific calculation library for Python',   
   author = 'Kubilay AYTEMIZ',                   
   author_email = 'meneskaytemiz@hotmail.com',      
-  url = 'https://github.com/Kobemeka/knopy',   
-  download_url = 'https://github.com/Kobemeka/knopy/archive/v0.0.0.tar.gz',    
+  url = 'https://github.com/Kobemeka/scicalc',   
+  download_url = 'https://github.com/Kobemeka/scicalc/archive/v0.tar.gz',    
   keywords = ['Science','Math','Physics','Chemistry','Calculation', 'Simulation'],   
   install_requires=[            
           'numpy',
